e486a4467f33894bf62f4d02e7183efd0bdead23
MJL <675531696@qq.com>
2020-10-23 18:38:31 +0800

    update readme

--- builder/flutter.js
+++ builder/flutter.js
@@ -114,7 +114,7 @@ async function makeflutter(flutterProjectPath = process.cwd()) {
 
 // 下划线转换驼峰
 function toHump(name) {
-  return name.replace(/[\_\-\+:\(\)\[\]](\w)/g, function (all, letter) {
+  return name.replace(/[\_\-\+:\(\)\[\] ](\w)/g, function (all, letter) {
     return letter.toUpperCase();
   });
 }
